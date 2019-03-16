23880f052a38bf26a19131dd21e0236c51b078bf
Martín M <skydiver@users.noreply.github.com>
2019-03-16 15:48:26 -0300

    breadcrumbs component

---
+++ components/Breadcrumbs.vue
@@ -0,0 +1,10 @@
+<template>
+  <nav class="breadcrumb" aria-label="breadcrumbs">
+    <ul>
+      <li><a href="../">Bulma</a></li>
+      <li><a href="../">Templates</a></li>
+      <li><a href="../">Examples</a></li>
+      <li class="is-active"><a href="#" aria-current="page">Admin</a></li>
+    </ul>
+  </nav>
+</template>
