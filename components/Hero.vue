e6a7de4af06a4b629fffc7a666080866eaea2454
Martín M <skydiver@users.noreply.github.com>
2019-03-16 15:48:31 -0300

    hero component

---
+++ components/Hero.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="hero is-info welcome is-small">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          Hello, Admin.
+        </h1>
+        <h2 class="subtitle">
+          I hope you are having a great day!
+        </h2>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.hero.welcome.is-info {
+  background: #36d1dc;
+  background: -webkit-linear-gradient(to right, #5b86e5, #36d1dc);
+  background: linear-gradient(to right, #5b86e5, #36d1dc);
+}
+.hero.welcome .title,
+.hero.welcome .subtitle {
+  color: hsl(192, 17%, 99%);
+}
+</style>
