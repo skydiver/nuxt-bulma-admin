7abcee02cbad2fba02f0bbd254eb924b68c89522
Martín M <skydiver@users.noreply.github.com>
2019-03-16 15:54:30 -0300

    table component

---
+++ components/Table.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="card events-card">
+    <header class="card-header">
+      <p class="card-header-title">
+        Events
+      </p>
+      <a href="#" class="card-header-icon" aria-label="more options">
+        <span class="icon">
+          <i class="fa fa-angle-down" aria-hidden="true"></i>
+        </span>
+      </a>
+    </header>
+    <div class="card-table">
+      <div class="content">
+        <table class="table is-fullwidth is-striped">
+          <tbody>
+            <tr>
+              <td width="5%"><i class="fa fa-bell-o"></i></td>
+              <td>Lorum ipsum dolem aire</td>
+              <td><a class="button is-small is-primary" href="#">Action</a></td>
+            </tr>
+            <tr>
+              <td width="5%"><i class="fa fa-bell-o"></i></td>
+              <td>Lorum ipsum dolem aire</td>
+              <td><a class="button is-small is-primary" href="#">Action</a></td>
+            </tr>
+            <tr>
+              <td width="5%"><i class="fa fa-bell-o"></i></td>
+              <td>Lorum ipsum dolem aire</td>
+              <td>
+                <a class="button is-small is-primary" href="#">Action</a>
+              </td>
+            </tr>
+            <tr>
+              <td width="5%"><i class="fa fa-bell-o"></i></td>
+              <td>Lorum ipsum dolem aire</td>
+              <td><a class="button is-small is-primary" href="#">Action</a></td>
+            </tr>
+            <tr>
+              <td width="5%"><i class="fa fa-bell-o"></i></td>
+              <td>Lorum ipsum dolem aire</td>
+              <td><a class="button is-small is-primary" href="#">Action</a></td>
+            </tr>
+            <tr>
+              <td width="5%"><i class="fa fa-bell-o"></i></td>
+              <td>Lorum ipsum dolem aire</td>
+              <td><a class="button is-small is-primary" href="#">Action</a></td>
+            </tr>
+            <tr>
+              <td width="5%"><i class="fa fa-bell-o"></i></td>
+              <td>Lorum ipsum dolem aire</td>
+              <td><a class="button is-small is-primary" href="#">Action</a></td>
+            </tr>
+            <tr>
+              <td width="5%"><i class="fa fa-bell-o"></i></td>
+              <td>Lorum ipsum dolem aire</td>
+              <td><a class="button is-small is-primary" href="#">Action</a></td>
+            </tr>
+            <tr>
+              <td width="5%"><i class="fa fa-bell-o"></i></td>
+              <td>Lorum ipsum dolem aire</td>
+              <td><a class="button is-small is-primary" href="#">Action</a></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <footer class="card-footer">
+      <a href="#" class="card-footer-item">View All</a>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.card .content {
+  font-size: 14px;
+}
+.card-footer-item {
+  font-size: 14px;
+  font-weight: 700;
+  color: #8f99a3;
+}
+.card-footer-item:hover {
+}
+.card-table .table {
+  margin-bottom: 0;
+}
+.events-card .card-table {
+  max-height: 250px;
+  overflow-y: scroll;
+}
+</style>
